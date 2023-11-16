fb2e4f86f780c6772351ec937529503f642c150f
Prem Singh <22cspre036@ldce.ac.in>
2023-11-16 18:24:24 +0530

    evening error commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 function App() {
   return (
+<<<<<<< Updated upstream
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,6 +20,14 @@ function App() {
         </a>
       </header>
     </div>
+=======
+    <>
+    
+    <Login></Login>
+   
+
+    </>
+>>>>>>> Stashed changes
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+<<<<<<< Updated upstream
 
+=======
+import "./global.css";
+import { BrowserRouter } from 'react-router-dom';
+import { StoreProvider } from './context/store';
+>>>>>>> Stashed changes
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <StoreProvider>
+        <App />
+      </StoreProvider>
+    </BrowserRouter>
+
   </React.StrictMode>
 );
 
